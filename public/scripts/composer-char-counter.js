f3dd60f83106759b56d3e37a27e70c936b81eb07
Michaela-K <michaelakodes@gmail.com>
2022-03-07 23:43:11 +0000

    add character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,29 @@
+$(() => {
+  function set(){
+    let typedText = $(this).val().length;
+    let maxChar = 140;
+    let remainingChars = maxChar - typedText;
+
+    let count = $(this).parent().children("div").children(".counter");
+    count.text(remainingChars);
+    console.log(count)
+
+    if (remainingChars < 0) {
+      count.addClass("red");
+    } else {
+      count.removeClass("red");
+    }
+  }
+
+    $("#tweet-text").keyup(set);
+
+});
+
+$(() => {
+  function reloadChar(){
+    let maxChar = 140;
+    $("output.counter").text(maxChar);
+    console.log("submit button clicked")
+  }
+  $(".submit").click(reloadChar);
+});
